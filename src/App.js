d381c94b8a50bcdac885f6affc4b2420c49ddf6e
fotis tsakiris <footios76@gmail.com>
2020-05-19 15:14:39 +0300

    useEffect Hook

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import React, { useState, useEffect } from "react";
 import { useForm } from "./useForm";
 
 import Hello from "./Hello";
+import { useFetch } from "./useFetch";
 const App = () => {
   const [values, handleChange] = useForm({
     email: "",
     password: "",
     firstName: ""
   });
-  const [showHello, setShowHello] = useState(true);
+  // use the updater function to not call JSON... in every render.
+  const [count, setCount] = useState(() => {
+    const number = JSON.parse(localStorage.getItem("count"));
+    if (number === null) {
+      return 0;
+    }
+    return number;
+  });
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
 
   useEffect(() => {
-    const onMouseMove = e => {
-      console.log(e);
-    };
-    console.log("App render");
-    window.addEventListener("mousemove", onMouseMove);
-    return () => {
-      console.log("App unnount");
-      window.removeEventListener("mousemove", onMouseMove);
-    };
-  }, [values.email]);
-
+    localStorage.setItem("count", JSON.stringify(count));
+  });
   return (
     <div className="App">
-      <button onClick={() => setShowHello(!showHello)}>toggle Hello</button>
-      {showHello && <Hello />}
+      <div>{!data ? "loading..." : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(c => c + 1)}>increment</button>
+      {/* <button onClick={() => setShowHello(!showHello)}>toggle Hello</button> */}
+      {/* {showHello && <Hello />} */}
       <input
         placeholder="email"
         name="email"
---
+++ src/useFetch.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+export const useFetch = url => {
+  const [state, setState] = useState({ data: null, loading: true });
+
+  useEffect(() => {
+    setState(state => ({ data: state.data, loading: true }));
+    fetch(url)
+      .then(x => x.text())
+      .then(y => {
+        console.log(y);
+        setState({ data: y, loading: false });
+      });
+  }, [url, setState]);
+
+  return state;
+};
