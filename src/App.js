9dae9adcc75425adba5fbdea8692f614b357fa55
fotis tsakiris <footios76@gmail.com>
2020-05-19 12:49:15 +0300

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useForm } from "./useForm";
 
-function App() {
+const App = () => {
+  const [values, handleChange] = useForm({ email: "", password: "" });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input name="email" value={values.email} onChange={handleChange} />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+        // onChange={e => setPassword(e.target.value)}
+      />
     </div>
   );
-}
+};
 
 export default App;
